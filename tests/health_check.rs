beda1e1180ac9d27713fc28f20469db2a7ef436c
ant <desertdev.iwu9v@aleeas.com>
2023-05-23 21:06:36 -0700

    Formatting

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,4 @@
-use std::{net::TcpListener};
-
+use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_works() {
@@ -15,9 +14,8 @@ async fn health_check_works() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 
-    fn spawn_app()-> String {
-        let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind to random port");
+    fn spawn_app() -> String {
+        let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
         let port = listener.local_addr().unwrap().port();
         let server = zero2prod::run(listener).expect("Failed to bind address");
         let _ = tokio::spawn(server);
